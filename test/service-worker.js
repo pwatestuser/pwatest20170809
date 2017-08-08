8b396ba14d07d9b50e65f593dc12d357256652b2
pwatestuser <30769787+pwatestuser@users.noreply.github.com>
2017-08-09 04:36:05 +0900

    a

---
+++ test/service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener("push", function(event){
+  event.waitUntil().then(function(res){
+    self.registration.showNotification("プッシュタイトル", {
+      body: "プッシュ通知",
+      icon: "/images/icons/icon-144x144.png",
+      tag: "tag"
+    });
+  });
+});
+self.addEventListener("notificationclick", function(event) {
+  event.notification.close();
+  clients.openWindow("/");
+}, false);
